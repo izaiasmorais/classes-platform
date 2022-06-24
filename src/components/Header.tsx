2e9f7f05c3aefd71273ac21151851fb7211790ad
Izaias Lima <53953937+IzaiasMorais@users.noreply.github.com>
2022-06-24 14:04:56 -0300

    update-9

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,9 @@
-import { useNavigate } from "react-router-dom";
 import { PurpleLogo } from "./AssetsTsx/PurpleLogo";
 
 export function Header() {
-  const navigate = useNavigate();
-
-  function goToHome() {
-    navigate("/");
-  }
-
   return (
     <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
-      <div onClick={goToHome}>
-        <PurpleLogo />
-      </div>
+      <PurpleLogo />
     </header>
   );
 }
